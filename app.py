b1ca6cc5184e5581ad43b5420d4670cced8c3e4a
JonathanW82 <Jonwheway@live.co.uk>
2020-09-28 22:52:39 +0100

    added css and a landing image, stated sorting mobile view

--- app.py
+++ app.py
@@ -9,12 +9,22 @@ def index():
     return render_template("index.html", page_title="Home")
 
 
+@app.route('/wiki')
+def wiki():
+    return render_template("wiki.html", page_title="Wiki")
+
+
 @app.route('/about')
 def about():
     return render_template("about.html", page_title="About")
 
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html", page_title="Contact")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP', '127.0.0.1'),
             port=int(os.environ.get('PORT', 5000)),
-            debug=True) 
+            debug=True)
