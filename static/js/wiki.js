d7b4ba2d9c71c3d6d814951b2411a4da39efce1e
JonathanW82 <Jonwheway@live.co.uk>
2020-09-30 15:42:57 +0100

    added Javascript to the wiki page to allow the injection of the video section

--- static/js/wiki.js
+++ static/js/wiki.js
@@ -1,3 +1,5 @@
+/* This function starts when the page loads and enables the autoscroll funtion */ 
+
 $(document).ready(function(){
     // this Function runs the scroll to top icon.
     //Check to see if the window is top if not then display button
@@ -15,3 +17,33 @@ $(document).ready(function(){
         return false;
     });
 });
+
+/* This function will inject the video section into the wiki page for ph testing info */
+
+function showvideos(){
+    // this function injects the html with the videos and changes the attribute 
+    // of the show videos button to hide videos.
+    // It also changes the value of the button to a different name.
+
+    document.getElementById('videos').innerHTML = `<hr color="white"><div class="row"><div class="col-sm-6"><div class="soilphvideo">
+            <div>Torsi Wooldridge on soil PH</div><iframe class="soilphvideoplayer" src="https://www.youtube.com/embed/h-IsLZCmWEY" \ frameborder="1"
+                    allow="encrypted-media; picture-in-picture" allowfullscreen></iframe>
+            </div></div><div class="col-sm-6"><div class="soilphvideo"><div>How to use a PH meter</div>  
+            <iframe class="soilphvideoplayer" src="https://www.youtube.com/embed/Xu2qVNiG300" frameborder="1"
+                    allow="encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></div></div><hr color="white">`;
+
+    document.getElementById('showvidbutton').setAttribute('onclick','hidevideos()');
+    let butname = document.getElementById('showvidbutton');
+    butname.value = "Hide Videos";
+};
+
+/* This function will hide the video section on the wiki page */
+
+function hidevideos(){
+    // This function hides the videos and changes the button attribute back to showvideos onclick.
+    // It also changes the value of the button to a different name
+    document.getElementById('videos').innerHTML = "";
+    document.getElementById('showvidbutton').setAttribute('onclick','showvideos()');
+    let butname = document.getElementById('showvidbutton');
+    butname.value = "Show Videos";
+}
