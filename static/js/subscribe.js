3522615bc939c170bced877498268962e001be06
JonathanW82 <Jonwheway@live.co.uk>
2020-10-08 18:24:55 +0100

    moved some dies to make screen layout better on subscribe page

--- static/js/subscribe.js
+++ static/js/subscribe.js
@@ -12,9 +12,10 @@ observer.observe(target, { attributes : true, attributeFilter : ['style'] });
 
 var observer2 = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutationRecord) {
-        document.getElementById('autoRedirectMessage').innerHTML = `<p>Please Wait You Will be Automatically Redirected!</p>`;
+        document.getElementById('autoRedirectMessage').innerHTML = `<p>Please Wait You Will be Automatically Redirected!</p>
+        <p>Incase you are not automatically redirected please <a href="#" onclick="backbut()">Click Here!</a></p>`;
         document.getElementById('backbuttondiv').scrollIntoView({behavior: 'smooth'})
-        document.getElementById('backbuttondiv').innerHTML = `<p>Incase you are not automatically redirected please <a href="#" onclick="backbut()">Click Here!</a></hp>`;
+    
         document.getElementById('subscribeButton').innerHTML = "";
         autoRedirect();
     });    
