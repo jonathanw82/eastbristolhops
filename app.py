fef27293cd5302def402ef473464d11359db05ca
JonathanW82 <Jonwheway@live.co.uk>
2020-10-08 20:46:00 +0100

    removed debug

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def subscribe():
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', 5000)),
-            debug=True)
+            debug=False)
